cfb40f5f873877afabcd7536200c6f7015754184
Dipender16 <110554467+Dipender16@users.noreply.github.com>
2024-10-28 14:20:15 +0530

    BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int arr[] = new int[N];
+
+        // Input of array
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // bubble sort in descending order
+        bubbleSort(N, arr);
+        // printing array elements
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+    public static void bubbleSort(int N, int[] arr) {
+        // your code here
+
+        for(int i = 0; i < N; i++){
+            int barier = N - i - 1;
+
+            for(int j = 0; j < barier; j++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
